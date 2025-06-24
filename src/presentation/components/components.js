2e2b306ed73a9fdb942688a707c47a42d9e7a42f
SabianF <58588133+SabianF@users.noreply.github.com>
2025-06-24 00:19:03 -0700

    refactor: rendering use 1 variable

--- src/presentation/components/components.js
+++ src/presentation/components/components.js
@@ -10,9 +10,10 @@ const BASE_PATH = "src/presentation/components";
  * @param {string} props.id
  */
 export async function renderComponent(component_name, props) {
-  const rendered_css = await getAndRenderCss(BASE_PATH + `/${component_name}.css`, props);
-  const rendered_js = await getAndRenderJs(BASE_PATH + `/${component_name}_client.js`, props);
-  const rendered_component = await getAndRenderHtml(BASE_PATH + `/${component_name}.html`, props);
+  const prefixed_component_name = `${BASE_PATH}/${component_name}`;
+  const rendered_css = await getAndRenderCss(prefixed_component_name + ".css", props);
+  const rendered_js = await getAndRenderJs(prefixed_component_name + "_client.js", props);
+  const rendered_component = await getAndRenderHtml(prefixed_component_name + ".html", props);
 
   return rendered_css + rendered_component + rendered_js;
 }
--- src/presentation/pages/pages.js
+++ src/presentation/pages/pages.js
@@ -11,9 +11,10 @@ const BASE_PATH = "src/presentation/pages";
  * @param {string} props.id
  */
 export async function renderPage(page_name, props) {
-  const rendered_css = await getAndRenderCss(BASE_PATH + `./${page_name}.css`, props);
-  const rendered_js = await getAndRenderJs(BASE_PATH + `./${page_name}_client.js`, props);
-  const rendered_page = await getAndRenderHtml(BASE_PATH + `/${page_name}.html`, props);
+  const prefixed_page_name = `${BASE_PATH}/${page_name}`;
+  const rendered_css = await getAndRenderCss(prefixed_page_name + ".css", props);
+  const rendered_js = await getAndRenderJs(prefixed_page_name + "_client.js", props);
+  const rendered_page = await getAndRenderHtml(prefixed_page_name + ".html", props);
 
   return rendered_css + rendered_page + rendered_js;
 }
