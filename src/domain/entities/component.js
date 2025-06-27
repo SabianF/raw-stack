379827147827b293c9d16e95bd8ec92745b15188
SabianF <58588133+SabianF@users.noreply.github.com>
2025-06-27 16:01:05 -0700

    test: Component.render

--- src/domain/entities/component.js
+++ src/domain/entities/component.js
@@ -41,7 +41,7 @@ class Component {
   props;
 
   async render() {
-
+    return "";
   }
 }
 
--- src/domain/entities/component.test.js
+++ src/domain/entities/component.test.js
@@ -1,11 +1,13 @@
 import component from "./component.js";
 
-describe(component.Component.name, () => {
+describe(component.Component.name, async () => {
+
   const test_component = new component.Component();
 
-  describe(test_component.render, () => {
-    test("returns string", () => {
-      const html = test_component.render();
+  describe(test_component.render, async () => {
+
+    test("returns string", async () => {
+      const html = await test_component.render();
       expect(typeof html).toEqual("string");
     });
   });
