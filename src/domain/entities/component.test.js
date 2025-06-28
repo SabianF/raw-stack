3469341cafa2b4527641c78b4e4fd8517ea18a94
SabianF <58588133+SabianF@users.noreply.github.com>
2025-06-27 17:42:20 -0700

    test: crap ton of render stuff

--- src/domain/entities/component.test.js
+++ src/domain/entities/component.test.js
@@ -4,7 +4,7 @@ describe(component.Component.name, () => {
 
   describe("constructor", () => {
 
-    describe.each([
+    const invalid_inputs = [
       {
         name: undefined,
       },
@@ -20,15 +20,9 @@ describe(component.Component.name, () => {
       {
         name: "name-with-hyphens",
       },
-    ])("throws on invalid name", (invalid_input) => {
-      test(JSON.stringify(invalid_input.name), async () => {
-        expect(() => {
-          new component.Component(invalid_input.name);
-        }).toThrow(Error);
-      });
-    });
+    ];
 
-    describe.each([
+    const valid_inputs = [
       {
         name: "button",
       },
@@ -38,11 +32,45 @@ describe(component.Component.name, () => {
       {
         name: "hubba_bubba",
       },
-    ])("returns string on valid name", (valid_input) => {
-      test(JSON.stringify(valid_input.name), async () => {
+    ];
+
+    describe.each(invalid_inputs)("on invalid input", (invalid_input) => {
+      test(`[${JSON.stringify(invalid_input.name)}] throws Error`, async () => {
+        expect(() => {
+          new component.Component(invalid_input.name);
+        }).toThrow(Error);
+      });
+    });
+
+    describe.each(valid_inputs)("on valid input", (valid_input) => {
+
+      test(`[${JSON.stringify(valid_input)}] returns Component`, async () => {
         const comp = new component.Component(valid_input.name);
         expect(comp.constructor.name).toBe(component.Component.name);
-        expect(comp.name).toBe(valid_input.name);
+      });
+
+      test("assigns name to Component.name", () => {
+        const comp = new component.Component(valid_input.name);
+        expect(comp.name).toEqual(valid_input.name);
+      });
+    });
+
+  });
+
+  describe(component.Component.prototype.render.name, () => {
+    const invalid_components = [
+      new component.Component(`definitely_does_not_exist_${crypto.randomUUID().replaceAll("-", "_")}`),
+    ];
+
+    const valid_components = [
+      new component.Component("layout"),
+    ];
+
+    describe("throws on invalid component", (invalid_component) => {
+      test.each(invalid_components)(JSON.stringify(invalid_component), async () => {
+        expect(() => {
+          invalid_component.render();
+        }).toThrow();
       });
     });
   });
