d628144f8e0074568a7b80049f8f9e2bacf79a58
SabianF <58588133+SabianF@users.noreply.github.com>
2025-06-27 21:00:25 -0700

    feat: sidebar nav

--- src/presentation/components/layout.js
+++ src/presentation/components/layout.js
@@ -30,18 +30,18 @@ export default async function layout(props) {
     props.custom_nav = default_nav;
   }
 
-  const default_css = [
-    `<link rel="stylesheet" href="css/pico.classless.min.css">`,
-    await getAndRenderCss("src/presentation/templates/components/layout_critical.css", props),
+  const rendered_layout_css = [
+    await getAndRenderCss("src/presentation/components/layout_critical.css", props),
   ].join("");
 
-
-  const rendered_layout_css = await getAndRenderCss(`./${layout_name}.css`, props);
   if (!props.critical_css) {
-    props.critical_css = default_css + rendered_layout_css;
+    props.critical_css = rendered_layout_css;
   } else {
     props.critical_css = rendered_layout_css + props.critical_css;
   }
+
+  console.log("rendered_layout_css:");
+  console.log(rendered_layout_css);
 
   const default_scripts = [
     await getAndRenderJs(`./${layout_name}_client.js`, props),
